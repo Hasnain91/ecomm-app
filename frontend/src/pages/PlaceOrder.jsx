a4b54b1357b8ad87e798607989068b5de50d314f
Hasnain Iftikhar <hasnainrao@yahoo.com>
2025-03-05 11:29:49 +0500

    place order

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,7 +1,151 @@
-import React from "react";
+import React, { useState } from "react";
+import CartTotal from "../components/CartTotal";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const PlaceOrder = () => {
-  return <div>PlaceOrder</div>;
+  const [paymentMethod, setPaymentMethod] = useState("cod");
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
+      {/* LEFT Side */}
+      <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
+        <div className="text-xl sm:text-2xl my-3">
+          <div className="inline-flex gap-2 items-center mb-3">
+            <p className="text-gray-500">
+              DELIVERY
+              <span className="text-gray-700 font-medium"> INFORMATION</span>
+            </p>
+            <p className="w-8 sm:w-12 h-[1.5px] sm:h-[2px] bg-gray-700"></p>
+          </div>
+        </div>
+        <div className="flex gap-3">
+          <input
+            type="text"
+            placeholder="First Name"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+        </div>
+        <input
+          type="email"
+          placeholder="Email Address"
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+        />
+        <input
+          type="text"
+          placeholder="Street Address"
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+        />
+        <div className="flex gap-3">
+          <input
+            type="text"
+            placeholder="City"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+          <input
+            type="text"
+            placeholder="State"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+        </div>
+        <div className="flex gap-3">
+          <input
+            type="number"
+            placeholder="Zip Code"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+          <input
+            type="text"
+            placeholder="Country"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+        </div>
+        <input
+          type="number"
+          placeholder="Phone"
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+        />
+      </div>
+
+      {/* RIGHT Side */}
+      <div className="mt-8">
+        <div className="mt-8 min-w-90">
+          <CartTotal />
+        </div>
+
+        <div className="mt-12">
+          <div className="inline-flex gap-2 items-center mb-3">
+            <p className="text-gray-500">
+              PAYMENT <span className="text-gray-700 font-medium">METHOD</span>
+            </p>
+            <p className="w-8 sm:w-12 h-[1.5px] sm:h-[2px] bg-gray-700"></p>
+          </div>
+
+          {/* Payment methods selection */}
+          <div className="flex flex-col lg:flex-row gap-3">
+            <div
+              onClick={() => setPaymentMethod("stripe")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-4.5 h-4.5 border rounded-full ${
+                  paymentMethod === "stripe" ? "bg-green-400" : ""
+                }`}
+              ></p>
+              <img
+                src={assets.stripe_logo}
+                alt="Stripe Logo"
+                className="h-5 mx-4"
+              />
+            </div>
+            {/* <div
+              onClick={() => setPaymentMethod("razorpay")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-4.5 h-4.5 border rounded-full ${
+                  paymentMethod === "razorpay" ? "bg-green-400" : ""
+                }`}
+              ></p>
+              <img
+                src={assets.razorpay_logo}
+                alt="Stripe Logo"
+                className="h-5 mx-4"
+              />
+            </div> */}
+            <div
+              onClick={() => setPaymentMethod("cod")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-4.5 h-4.5 border rounded-full ${
+                  paymentMethod === "cod" ? "bg-green-400" : ""
+                }`}
+              ></p>
+              <p className="text-gray-500 text-md font-medium">
+                CASH ON DELIVERY
+              </p>
+            </div>
+          </div>
+
+          <div className="w-full text-end mt-8">
+            <button
+              onClick={() => navigate("/orders")}
+              className="bg-black text-white px-16 py-3  text-md font-medium cursor-pointer hover:bg-gray-300 hover:text-black"
+            >
+              PLACE ORDER
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PlaceOrder;
