27a424ad396b2e98da69693c12bbc46324b8694a
Hasnain Iftikhar <hasnainrao@yahoo.com>
2025-03-08 16:38:41 +0500

    fetch user's order from db

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,8 +1,49 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Orders = () => {
-  const { currency, products } = useContext(ShopContext);
+  const { currency, backendUrl, token } = useContext(ShopContext);
+  const [orders, setOrders] = useState([]);
+
+  const fetchOrders = async () => {
+    try {
+      if (!token) {
+        return null;
+      }
+
+      const res = await axios.post(
+        `${backendUrl}/api/order/user-orders`,
+        {},
+        { headers: { token } }
+      );
+
+      // console.log("Response for fetching orders: ", res.data);
+      if (res.data.success) {
+        let allOrders = [];
+        res.data.orders.map((order) => {
+          order.items.map((item) => {
+            item["status"] = order.status;
+            item["payment"] = order.payment;
+            item["paymentMethod"] = order.paymentMethod;
+            item["date"] = order.date;
+
+            allOrders.push(item);
+          });
+        });
+        setOrders(allOrders.reverse());
+        console.log(allOrders);
+      }
+    } catch (error) {
+      console.log("Error in fetchOrders: ", error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchOrders();
+  }, [token]);
 
   return (
     <div className="border-t pt-16">
@@ -16,7 +57,7 @@ const Orders = () => {
       </div>
 
       <div>
-        {products.slice(1, 4).map((product, index) => (
+        {orders.map((product, index) => (
           <div
             key={index}
             className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4"
@@ -25,16 +66,27 @@ const Orders = () => {
               <img className="w-16 sm:w-20" src={product.image[0]} alt="" />
               <div>
                 <p className="sm:text-base font-medium">{product.name}</p>
-                <div className="flex items-center gap-3 mt-2 text-base text-gray-700">
-                  <p className="text-lg">
+                <div className="flex items-center gap-3 mt-1 text-base text-gray-700">
+                  <p>
                     {currency}
                     {product.price}
                   </p>
-                  <p>Quantity: 1</p>
-                  <p>Size: M</p>
+                  <p>Quantity: {product.quantity}</p>
+                  <p>Size: {product.size}</p>
                 </div>
-                <p>
-                  Date: <span className="text-gray-400">05 Mar, 2025</span>
+                <p className="mt-1">
+                  Date:
+                  <span className="text-gray-400">
+                    {" "}
+                    {new Date(product.date).toDateString()}
+                  </span>
+                </p>
+                <p className="mt-1">
+                  Payment:
+                  <span className="text-gray-400">
+                    {" "}
+                    {product.paymentMethod}
+                  </span>
                 </p>
               </div>
             </div>
@@ -42,9 +94,12 @@ const Orders = () => {
             <div className="md:w-1/2 flex justify-between">
               <div className="flex items-center gap-3">
                 <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
-                <p className="text-sm md:text-base  ">READY TO SHIP</p>
+                <p className="text-sm md:text-base  ">{product.status}</p>
               </div>
-              <button className="border px-4 py-2 text-sm font-medium rounded-sm cursor-pointer">
+              <button
+                onClick={fetchOrders}
+                className="border px-4 py-2 text-sm font-medium rounded-sm cursor-pointer"
+              >
                 Track Order
               </button>
             </div>
